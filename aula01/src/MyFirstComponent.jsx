50c510bf934c91115fa0c27a9ac2b78811037afb
EoTavinho <149340850+EoTavinho@users.noreply.github.com>
2025-05-07 17:53:12 -0300

    react.js

---
+++ aula01/src/MyFirstComponent.jsx
@@ -0,0 +1,17 @@
+function MyFirstComponent(){
+    const nome = 'Tavinho'
+
+    return(
+        <>
+        <div>
+            <h1>Hello World!!!</h1>
+        </div>
+        <div>
+            <p>Seu nome é {nome}</p>
+            <p>a soma de 2+2 é {2+2}</p>
+        </div>
+        </>
+    )
+}
+
+export default MyFirstComponent
