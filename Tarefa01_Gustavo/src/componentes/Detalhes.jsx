7c9f90f6f972b4b64f1edb6a527c8e2f50b1d54f
EoTavinho <149340850+EoTavinho@users.noreply.github.com>
2025-05-22 22:37:18 -0300

    finalizado

---
+++ Tarefa01_Gustavo/src/componentes/Detalhes.jsx
@@ -0,0 +1,13 @@
+import './Detalhes.css';
+
+export default function Detalhes({ personagem }) {
+  if (!personagem) return null;
+
+  return (
+    <div className="detalhes">
+      <h2>{personagem.nome}</h2>
+      <img src={personagem.imagem} alt={personagem.nome} />
+      <p>{personagem.descricao}</p>
+    </div>
+  );
+}
