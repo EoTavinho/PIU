bfeef8e956c29375a878976dd5a9d1f363973255
EoTavinho <149340850+EoTavinho@users.noreply.github.com>
2025-08-03 19:01:12 -0300

    questao 1 finalizada

--- Gustavo_Henrique_Prova02/src/componentes/FormCor.jsx
+++ Gustavo_Henrique_Prova02/src/componentes/FormCor.jsx
@@ -2,52 +2,62 @@ import { useState, useEffect } from "react";
 
 
 export default function FormCor() {
-    const [cor, setCor] = useState('light')
-    const [select, setSelect] = useState()
-    const [nome , setNome] = useState()
+    const [cor, setCor] = useState(null)
+    const [select, setSelect] = useState('padrao')
+    const [nome , setNome] = useState('')
+    const [mostrarDados, setMostrarDados] = useState(false)
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setCor(cor)
+        setCor(select)
+        setMostrarDados(true)
+        setSelect('padrao')
+        
     }
 
-    const handleColor = () =>{
-        
-        if (select === 'blue') {
+    
+    useEffect(() => {
+      if (cor === 'blue') {
             document.body.style.backgroundColor = 'blue'
-        } else {
+        } else if (cor === 'padrao') {
+            document.body.style.backgroundColor = ''
+        } else if (cor === 'red') {
             document.body.style.backgroundColor = 'red'
         }
-    }
-    useEffect(() => {
-        handleColor()
-    }, [select])
+    }, [cor])
     
     return(
         <div>
             <h1>Formulario</h1>
-            <form onSubmit={handleSubmit}>
+             <form onSubmit={handleSubmit}>
                 <label>Digite seu nome: </label>
                 <input 
                     type="text"
                     value={nome}
                     onChange={(e) => setNome(e.target.value)}
                     placeholder="Nome"
-                 />
-                
+                 /> <br /> <br />
+                 
                 <label>Selecione uma opção de cor:
                     <select name="selecione" onChange={(e) => setSelect(e.target.value)}>
-                        <option value="azul">Azul</option>
-                        <option value="vermelho">Vermelho</option>
+                        <option value="padrao">{select}</option>
+                        <option value="blue">Azul</option>
+                        <option value="red">Vermelho</option>
                     </select>
                 </label>
-                
-                <button type='submit' onClick={handleColor}>Enviar</button>
+            
+                <button type='submit'>Enviar</button>
 
-                <p>{nome}</p>
-                <p>{select}</p>
 
             </form>
+
+            {mostrarDados && (
+                <div>
+                    <p>{nome}</p>
+                    <p>{cor}</p>
+                </div>
+            )}
+
         </div>
     )
 }
